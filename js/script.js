dcb163e3ebbf35b26a770e60851440ed20f33e0a
PatLiCoding <patricia.linne@yahoo.de>
2025-11-24 12:47:22 +0100

    add classes files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+let world = new World();
+
+function init() {
+  console.log(world.enemies);
+
+  //   character.src = "./assets/img/2_character_pepe/2_walk/W-21.png";
+}
