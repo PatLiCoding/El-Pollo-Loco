a7500aba8b7b849ce46673fb99a3ef2bc9bb8c63
PatLiCoding <patricia.linne@yahoo.de>
2025-11-25 13:34:04 +0100

    add animate clouds and character

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -1,9 +1,29 @@
 class Character extends MovableObject {
   y = 140;
   height = 300;
+  IMAGES_WALKING = [
+    "./assets/img/2_character_pepe/2_walk/W-21.png",
+    "./assets/img/2_character_pepe/2_walk/W-22.png",
+    "./assets/img/2_character_pepe/2_walk/W-23.png",
+    "./assets/img/2_character_pepe/2_walk/W-24.png",
+    "./assets/img/2_character_pepe/2_walk/W-25.png",
+    "./assets/img/2_character_pepe/2_walk/W-26.png",
+  ];
+  currentImage = 0;
 
   constructor() {
     super().loadImage("./assets/img/2_character_pepe/2_walk/W-21.png");
+    this.loadImages(this.IMAGES_WALKING);
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_WALKING.length;
+      let path = this.IMAGES_WALKING[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 100);
   }
 
   jump() {}
