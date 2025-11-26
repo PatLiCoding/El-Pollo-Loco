0203332234923631ae773d723f47a3e89d17a1b4
PatLiCoding <patricia.linne@yahoo.de>
2025-11-26 10:25:33 +0100

    add character and world optimization

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -1,22 +1,6 @@
 class World {
   character = new Character();
-  enemies = [new Chicken(), new Chicken(), new Chicken()];
-  clouds = [new Cloud()];
-  backgroundObjects = [
-    new BackgroundObject("./assets/img/5_background/layers/air.png", 0),
-    new BackgroundObject(
-      "./assets/img/5_background/layers/3_third_layer/1.png",
-      0
-    ),
-    new BackgroundObject(
-      "./assets/img/5_background/layers/2_second_layer/1.png",
-      0
-    ),
-    new BackgroundObject(
-      "./assets/img/5_background/layers/1_first_layer/1.png",
-      0
-    ),
-  ];
+  level = level1;
   canvas;
   ctx;
   keyboard;
@@ -34,10 +18,10 @@ class World {
 
     this.ctx.translate(this.camera_x, 0);
 
-    this.addObjectToMap(this.backgroundObjects);
-    this.addObjectToMap(this.clouds);
+    this.addObjectToMap(this.level.backgroundObjects);
+    this.addObjectToMap(this.level.clouds);
     this.addToMap(this.character);
-    this.addObjectToMap(this.enemies);
+    this.addObjectToMap(this.level.enemies);
 
     this.ctx.translate(-this.camera_x, 0);
 
