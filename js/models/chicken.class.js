b793e1d73068e87faed925d387d7ca535fb47162
PatLiCoding <patricia.linne@yahoo.de>
2025-11-26 08:23:12 +0100

    add chicken animate

--- js/models/chicken.class.js
+++ js/models/chicken.class.js
@@ -4,12 +4,29 @@ class Chicken extends MovableObject {
   img;
   height = 60;
   width = 40;
+  IMAGES_WALKING = [
+    "./assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+    "./assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+    "./assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+  ];
+  currentImage = 0;
 
   constructor() {
     super().loadImage(
       "./assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png"
     );
+    this.loadImages(this.IMAGES_WALKING);
 
+    this.animate();
     this.x = 200 + Math.random() * 500;
+  }
+
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_WALKING.length;
+      let path = this.IMAGES_WALKING[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 100);
   }
 }
